bcec4ed9ae35bbb34b015579526690957f3bec4e
Farhan Tahmid <farhantahmid881@gmail.com>
2023-11-27 20:29:46 +0600

    model userdata

--- UserData/models.py
+++ UserData/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+    email=models.EmailField(null=False,blank=False)
+    username=models.CharField(primary_key=True,null=False,blank=False,max_length=20)
+    
+    class Meta:
+        verbose_name="User Information"
+    
+    def __str__(self) -> str:
+        return self.username
